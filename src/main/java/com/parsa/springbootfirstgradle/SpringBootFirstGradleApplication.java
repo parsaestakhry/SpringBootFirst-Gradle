39bc89ff907f934ca8625be179188683dc81272c
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 20:08:42 +0330

    retrieve Alien bean and invoke coding method in main application

--- src/main/java/com/parsa/springbootfirstgradle/SpringBootFirstGradleApplication.java
+++ src/main/java/com/parsa/springbootfirstgradle/SpringBootFirstGradleApplication.java
@@ -2,13 +2,20 @@ package com.parsa.springbootfirstgradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+
+//any object created by spring is called : bean
 @SpringBootApplication
 public class SpringBootFirstGradleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootFirstGradleApplication.class, args);
-        System.out.println("Hello world");
+        ApplicationContext context = SpringApplication.run(SpringBootFirstGradleApplication.class, args);
+
+//        we want to let spring to create the object for us
+        Alien alien = context.getBean(Alien.class);
+        alien.code();
+
     }
 
 }
