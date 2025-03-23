d6db7986723c1d5976d146ba6231b336d9689f02
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 20:22:02 +0330

    inject CPU dependency into Laptop class and update compile method to use doingSomething

--- src/main/java/com/parsa/springbootfirstgradle/Laptop.java
+++ src/main/java/com/parsa/springbootfirstgradle/Laptop.java
@@ -1,11 +1,14 @@
 package com.parsa.springbootfirstgradle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Laptop {
 
+    @Autowired
+    CPU cpu;
     public  void  compile(){
-        System.out.println("compiling");
+        cpu.doingSomething();
     }
 }
