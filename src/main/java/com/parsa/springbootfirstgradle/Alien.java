fb5f98d2686553fa757e2c649103183d4c889ff2
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 20:19:33 +0330

    inject Laptop bean into Alien class and update code method to call compile

--- src/main/java/com/parsa/springbootfirstgradle/Alien.java
+++ src/main/java/com/parsa/springbootfirstgradle/Alien.java
@@ -1,13 +1,17 @@
 package com.parsa.springbootfirstgradle;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 // telling spring to create the object
 @Component
 public class Alien {
+
+    @Autowired // means connect it to the bean which is the laptop class
+    Laptop laptop;
     public void code() {
-        System.out.println("coding");
+        laptop.compile();
     }
 }
