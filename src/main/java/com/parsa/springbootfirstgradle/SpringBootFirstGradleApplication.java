02b6ae0d98b71ae281cde6f0e79e695523345ab4
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 19:56:40 +0330

    initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/java/com/parsa/springbootfirstgradle/SpringBootFirstGradleApplication.java
@@ -0,0 +1,13 @@
+package com.parsa.springbootfirstgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFirstGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parsa/springbootfirstgradle/SpringBootFirstGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsa.springbootfirstgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
